01f953e9ff14b272ee2dd1027f85ca6c16d606e1
Joe Averbukh <joeaverbukh@gmail.com>
2014-03-13 23:51:34 -0700

    Hacky solution to run in both local and heroku

--- web/__init__.py
+++ web/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:////tmp/test.db')
 db = SQLAlchemy(app)
 
 from web import views, models
