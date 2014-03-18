989d40f2cb49ed8dcae9becb9d97b40148a1352b
Joe Averbukh <joeaverbukh@gmail.com>
2014-03-18 15:51:26 -0700

    Minor refactor publish.py

--- publish.py
+++ publish.py
@@ -7,8 +7,10 @@ from web.models import Post
 
 static_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'web/static/posts')
 
-def publish_post(post_name):
-    file_path = os.path.join(static_directory, post_name)
+
+def publish_post(args):
+    file_name = args.title.replace(' ', '_').lower() + ".txt"
+    file_path = os.path.join(static_directory, file_name)
 
     with open(file_path, 'r') as f:
         author = f.readline().strip('n')
@@ -32,13 +34,13 @@ def publish_post(post_name):
     new_post = Post(author=author, title=title, content=content)
     db.session.add(new_post)
     db.session.commit()
-    print "Added {} to the db!".format(post_name)
+    print "Added {} to the db!".format(title)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Tool for publishing posts")
     parser.set_defaults(func=publish_post)
 
-    parser.add_argument('post', help="Filename of post to publish")
+    parser.add_argument('title', type=clean_title, help="Title of post")
 
     args = parser.parse_args()
-    args.func(args.post)
+    args.func(args)
---
+++ util.py
@@ -0,0 +1,5 @@
+" Helper functions common for scripts "
+
+def clean_title(t):
+    "Used for pretty-formatting a blog title"
+    return t.capitalize()
