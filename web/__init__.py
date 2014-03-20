564a0afc328a7ba88e0618757f1c99783e37c4db
Joe Averbukh <joeaverbukh@gmail.com>
2014-03-20 02:11:14 -0700

    Major UI Improvements
    * Add Flask-Assets and dependencies to bundle and minify css
    * Create css folder in static directory and add basic stlying rules
    * Cleanup templates

--- web/__init__.py
+++ web/__init__.py
@@ -1,8 +1,23 @@
 import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.assets import Environment, Bundle
+
+def register_assets(app):
+    assets = Environment(app)
+    assets.url = app.static_url_path
+
+    CSS_ASSETS = [
+        'css/vendor/readable-bootstrap.css',
+        Bundle('css/application.scss', filters='pyscss', output='css/compiled-scss.css')
+    ]
+
+    css = Bundle(*CSS_ASSETS, filters='cssmin', output='css/bundle.min.css')
+    assets.register('scss_all', css)
+    return assets
 
 app = Flask(__name__)
+register_assets(app)
 
 ### TODO: Hacky, come up with a better way
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:////tmp/test.db')
