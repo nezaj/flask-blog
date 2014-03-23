741dea0de715489e91c91d7ca138167591cc33b0
Joe Averbukh <joeaverbukh@gmail.com>
2014-03-22 19:29:08 -0700

    Use slugs for URLs and render friendly time for posts

--- web/views.py
+++ web/views.py
@@ -3,24 +3,15 @@ from web import app
 from web.models import Post
 from markdown import markdown
 
-def clean_contents(c):
-    " Helper for cleaning contents of blog post "
-    def replace_newlines(s):
-        return s.replace('\n', '<br />')
-
-    c = replace_newlines(c)
-    return c
-
 @app.route("/")
 @app.route("/posts")
 def posts():
     " Displays a list of posts "
-    posts = Post.query.all()
+    posts = Post.query.order_by(Post.published_dt.desc())
     return render_template('/posts/index.tmpl', posts=posts)
 
-@app.route("/posts/<string:title>")
-def post(title):
+@app.route("/posts/<string:slug>")
+def post(slug):
     " Displays an individual post "
-    post = Post.query.filter_by(title=title).first_or_404()
-    content = clean_contents(post.content)
-    return render_template('/posts/show.tmpl', post=post, content=content)
+    post = Post.query.filter_by(slug=slug).first_or_404()
+    return render_template('/posts/show.tmpl', post=post)
