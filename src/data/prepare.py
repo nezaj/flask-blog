f788b8f57ca33a18cced7d11666d85f8bfe0e17c
Joe Averbukh <joeaverbukh@gmail.com>
2014-04-18 23:56:04 -0700

    Seperate rebuild into build and prepopulate

--- src/data/prepare.py
+++ src/data/prepare.py
@@ -1,25 +1,18 @@
-#!/usr/bin/env python
-
-"""
-Script to re-create the database and populate it with some sample posts
-"""
-
 from config import app_config
-from data.db import DatabaseConnection
 from data.models import Base
 from commands import publish_post, generate_post
 from test.generate_tools import generate_post as dummy_post
 from loggers import get_stderr_logger
 
 def prepopulate_db():
-
+    " Prepopulates db with sample data "
     logger = get_stderr_logger()
-
     for _ in xrange(0, 50):
         p = dummy_post()
         generate_post(p, logger, force=True)
         publish_post(p, logger)
 
+# TODO: I'm currently not using this, I should either use it or throw it out
 def stamp_db():
     """
     Creates alembic_version table if it doesn't already exist
@@ -31,19 +24,6 @@ def stamp_db():
     alembic_cfg = Config(app_config.ALEMBIC_INI_PATH)
     command.stamp(alembic_cfg, "head")
 
-def rebuild_db():
-    " Recreates the db and stamps it with the latest revision "
-    db_url = app_config.SQLALCHEMY_DATABASE_URI
-    db = DatabaseConnection(db_url)
-
-    Base.metadata.drop_all(db.engine)
-    Base.metadata.create_all(db.engine)
-
-if __name__ == '__main__':
-    rebuild_db()
-
-    if not app_config.ENV == 'prod':
-        stamp_db()
-        prepopulate_db()
-
-    print 'database rebuilt!'
+def build_db(engine):
+    " Generates db tables based on metadata schema "
+    Base.metadata.create_all(engine)
--- src/sql.py
+++ src/sql.py
@@ -5,7 +5,14 @@ import os
 import subprocess
 
 from config import app_config
+from data.db import DatabaseConnection
 from util import parse_sqlalchemy_url
+
+def yes_no(message):
+    response = raw_input("{} [y/n] ".format(message))
+    while response.lower() not in ['y', 'n']:
+        response = raw_input("Please enter 'y' or 'n'. ")
+    return response == 'y'
 
 def build_named_arglist(arg_dict):
     for name, value in arg_dict.iteritems():
@@ -14,6 +21,20 @@ def build_named_arglist(arg_dict):
 
 def invoke_process(proc_name, proc_args, **subprocess_args):
     return subprocess.call([proc_name] + proc_args, **subprocess_args)
+
+def build(args):
+    db = DatabaseConnection(args.url)
+    print u"Connected to {}.".format(repr(db.engine.url))
+    if yes_no("Are you sure you want to build the database?"):
+        from data.prepare import build_db
+        print "Building database..."
+        build_db(db.engine)
+
+def prepopulate(args):  # pylint: disable=W0613
+    if yes_no("Are you sure you want to prepopulate the database with sample data?"):
+        from data.prepare import prepopulate_db
+        print "Prepopulating databsae..."
+        prepopulate_db()
 
 def repl(args):
     dialect = args.url.get_dialect()
@@ -40,6 +61,16 @@ if __name__ == '__main__':
                         default=app_config.SQLALCHEMY_DATABASE_URI,
                         help="A RFC1738 URL to a PostgreSQL or SQLite database to use.")
     subparsers = parser.add_subparsers()
+
+    build_parser = subparsers.add_parser('build', description="""
+    Build the database with tables defined in metadata
+    """)
+    build_parser.set_defaults(func=build)
+
+    prepopulate_parser = subparsers.add_parser('prepopulate', description="""
+    Prepopulate the database with sample data
+    """)
+    prepopulate_parser.set_defaults(func=prepopulate)
 
     repl_parser = subparsers.add_parser('repl', description="""
     Launch a psql or sqlite repl connected to the database.
